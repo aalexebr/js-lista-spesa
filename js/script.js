b28e455bb4c7855fdfb74710fa15136be8f2f18b
aalexebr <alex.ebr@icloud.com>
2023-06-19 14:18:35 +0200

    added createElement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-const list = [
+let list = [
     'a',
     'b',
     'c',
     'd'
 ]
+console.log('list array', list)
 
+let ul = document.createElement("ul");
+document.body.appendChild(ul);
+
+// cycle variable
+let i = 0
+while (i < list.length){
+    ul.innerHTML += `
+        <li>
+            ${list[i]}
+        </li>
+    `
+    console.log('list array', list[i])
+    i++;
+}
