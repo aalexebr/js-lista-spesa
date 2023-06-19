5caa99b3114e1d8d459925fdf7d89ccc09ff9db4
aalexebr <alex.ebr@icloud.com>
2023-06-19 14:20:09 +0200

    created while cycle

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ let list = [
     'd'
 ]
 console.log('list array', list)
-
+// created ul for the cycle to create li based ff the array iterations
 let ul = document.createElement("ul");
 document.body.appendChild(ul);
 
 // cycle variable
-// let i = 0
-// while (i < list.length){
-//     ul.innerHTML += `
-//         <li>
-//             ${list[i]}
-//         </li>
-//     `
-//     console.log('list array', list[i])
-//     i++;
-// }
+let i = 0
+while (i < list.length){
+    // ul.innerHTML += `
+    //     <li>
+    //         ${list[i]}
+    //     </li>
+    // `
+    console.log('list array', list[i])
+    i++;
+}
