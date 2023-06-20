c95d1d1feb8acf08d093b94220657e30121aa80d
aalexebr <alex.ebr@icloud.com>
2023-06-20 09:04:59 +0200

    UPDATE-commented equivalent code

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-let list = [
+const list = [
     'a',
     'b',
     'c',
-    'd'
+    'd',
+    'e'
 ]
 console.log('list array', list)
 // created ul for the cycle to create li based ff the array iterations
-let ul = document.createElement("ul");
+const ul = document.createElement("ul");
 document.body.appendChild(ul);
 
 // cycle variable
 let i = 0
+// cycle with condition to limit its duration
 while (i < list.length){
     ul.innerHTML += `
         <li>
             ${list[i]}
         </li>
     `
-    console.log('list array', list[i])
+
+    // OR
+    // const li = document.createElement("li");
+    // ul.append(li)
+    // li.innerHTML = list[i]
+    // progression of iterations
     i++;
 }
